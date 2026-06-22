22aee03e782dc439dfd19510c46efad604fb27d6
ksenofonmaglousidis-a11y <ksenofonmaglousidis@gmail.com>
2026-06-22 20:40:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ const COOLDOWN_WITHOUT_ROLE_MINUTES = 240;
 const UNLIMITED_SPINS_ROLE_ID = "1480671765909733472";
 const ALLOWED_CHANNEL_ID = "1493149839805120602";
 
+const REQUIRED_REACTION_MESSAGE_ID =
+  process.env.REQUIRED_REACTION_MESSAGE_ID;
+const REQUIRED_REACTION_CHANNEL_ID =
+  process.env.REQUIRED_REACTION_CHANNEL_ID;
+
 const VOUCH_CHANNEL_ID = "1480671767939776740";
 const PROMO_ROLE_ID = "1494867239256719540";
 const PROMO_DOWNLOAD_LINK =
@@ -704,6 +709,43 @@ client.on("interactionCreate", async (interaction) => {
 
       await interaction.deferUpdate();
       await interaction.message.delete().catch(() => {});
+
+      try {
+        const reactionChannel = await interaction.guild.channels.fetch(
+          REQUIRED_REACTION_CHANNEL_ID
+        );
+
+        const targetMessage = await reactionChannel.messages.fetch(
+          REQUIRED_REACTION_MESSAGE_ID
+        );
+
+        let hasReacted = false;
+
+        for (const reaction of targetMessage.reactions.cache.values()) {
+          const users = await reaction.users.fetch();
+
+          if (users.has(interaction.user.id)) {
+            hasReacted = true;
+            break;
+          }
+        }
+
+        if (!hasReacted) {
+          await interaction.channel.send({
+            content: `${interaction.user} ❌ You must react in <#${REQUIRED_REACTION_CHANNEL_ID}> before using the spin. Please react there and try again.`
+          });
+
+          return;
+        }
+      } catch (err) {
+        console.error("Reaction check error:", err);
+
+        await interaction.channel.send({
+          content: `${interaction.user} ❌ Unable to verify your reaction right now. Please try again later.`
+        });
+
+        return;
+      }
 
       await processSpin({
         user: interaction.user,
